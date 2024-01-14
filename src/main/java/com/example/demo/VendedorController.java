74c0fa4f07b11214e68826fe7bc4385dc5c03507
Mateus Rampim <mateusrampim@gmail.com>
2024-01-14 15:08:47 -0300

    x

--- src/main/java/com/example/demo/VendedorController.java
+++ src/main/java/com/example/demo/VendedorController.java
@@ -10,7 +10,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/vendedores")
+@RequestMapping("/api/v1/vendedores" )
 public class VendedorController {
 
     @PostMapping
