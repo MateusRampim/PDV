0af438ae7f1532a7ea045489144aebcf89297b07
Mateus Rampim <mateusrampim@gmail.com>
2024-01-14 14:02:16 -0300

    base code

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.models.Vendedor;
+
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(Model model) {
+        String message = "Olá, mundo!";
+        model.addAttribute("message", message);
+        return "index";
+    }
+
+    @PostMapping("/enviar")
+    public String enviar(@RequestParam String nome, @RequestParam String contato, @RequestParam String sexo, Model model) {
+        model.addAttribute("nome", nome);
+        model.addAttribute("idade", contato);
+        model.addAttribute("sexo", sexo);
+        Vendedor vend = new Vendedor(nome,sexo);
+        return "resultado";
+    }
+}
